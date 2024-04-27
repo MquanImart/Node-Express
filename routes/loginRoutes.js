f856a0e2973803e523228f509412e88a0a318932
MquanImart <phanminhquanmh@gmail.com>
2024-04-27 21:59:53 +0700

    test

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -2,6 +2,5 @@ const express = require("express")
 const loginControllers = require('../controllers/loginController');
 const router = express.Router();
 
-router.route("/").get(loginControllers.checkUser);
-
+router.route("/").get(loginControllers.checkUser).post(loginControllers.getAllAccount);
 module.exports = router;
