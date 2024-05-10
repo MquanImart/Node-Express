1141f806a930a0f3b7916a34c5b946a6fc7c6f3f
Nguyen Thien Nhan <nhanthien.tnn@gmail.com>
2024-05-10 14:50:14 +0700

    commit

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -11,5 +11,20 @@ router.route("/addBook/").post(bookController.addBook);
 router.route("/deleteBook/").get(bookController.deleteBook)
 router.route("/getFavBooks").get(bookController.getFavBooks)
 router.route("/getRecentViewBooks").get(bookController.getRecentViewBooks)
+router.route("/imgSearch").post(bookController.imgSearch)
+router.route("/textSearch").post(bookController.textSearch)
 
+
+module.exports = router;
+const express = require("express")
+const bookController = require('../controllers/bookController');
+const router = express.Router();
+
+router.route("/getBook/").get(bookController.getBook);
+router.route("/getBookById/:id").get(bookController.getBookById);
+router.route("/addBook/").post(bookController.addBook);
+router.route("/deleteBook/").get(bookController.deleteBook)
+router.route("/getFavBooks").get(bookController.getFavBooks)
+router.route("/getRecentViewBooks").get(bookController.getRecentViewBooks)
+    
 module.exports = router;
