bcb38307b1c6bef2bfd500594dcde0ba9d93aba1
MquanImart <phanminhquanmh@gmail.com>
2024-05-18 11:28:06 +0700

    commit

--- models/user.js
+++ models/user.js
@@ -10,6 +10,9 @@ class User{
         let [result, _] = await db.execute(sql);
         result[0].dob = result[0].dob.toLocaleString();
         result[0].dob = result[0].dob.split(",")[0];
+        let [month, day, year] = result[0].dob.split("/"); // Tách các phần của ngày
+        let formattedDate = `${year}/${month.padStart(2, '0')}/${day.padStart(2, '0')}`; // Định dạng lại thành YYYY/MM/DD
+        result[0].dob = formattedDate;
         return result;
     }
     static async updateInfoSql(name, email, sdt, dob, gender, id){
